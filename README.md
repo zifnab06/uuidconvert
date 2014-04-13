0896f2db5a92fda6ac75e12f82ecb080e5cc2862
Tom Powell <zifnab@zifnab06.net>
2014-04-13 22:22:17 +0000

    Add optional prefix

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Install:
     python app.py
 
 Modify local_config to fit your need
+
+
+To get information from this api:
+
+To get the UUID: 
+
+    http://sexypenguins.at/uuidconvert/player/zifnab06/
+
+To get a playername from a UUID:
+
+    http://sexypenguins.at/uuidconvert/uuid/a73d8981ccd149e1b810cdc73cc101f1/
--- app.py
+++ app.py
@@ -7,16 +7,19 @@ with app.app_context():
     import local_config
     app.config.from_object(local_config)
     
-    @app.route('/player/<string:playername>/')
+    @app.route(app.config['PREFIX'] + '/player/<string:playername>/')
     def getUUID(playername):
         profiles = get_uuid(playername)
         if profiles is not None and len(profiles) > 0:
             return jsonify(profiles[0])
-    @app.route('/uuid/<string:uuid>/')
+    @app.route(app.config['PREFIX'] + '/uuid/<string:uuid>/')
     def getName(uuid):
         profile = get_profile(uuid)
         if profile is not None:
             return jsonify(profile)
+    @app.route(app.config['PREFIX'] + '/')
+    def main():
+        return 'Please see http://github.com/zifnab06/uuidconvert'
 
 if __name__ == "__main__":
     app.run(
